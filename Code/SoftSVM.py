8547ce1ec1b0ac3c275f33a026c4354f5aad3e69
Eyal Amdur <eyalamdur@gmail.com>
2025-01-03 20:32:39 +0200

    Finished Part 3

--- Code/SoftSVM.py
+++ Code/SoftSVM.py
@@ -44,13 +44,20 @@ class SoftSVM(BaseEstimator, ClassifierMixin):
         """
         margins = (X.dot(w) + b).reshape(-1, 1)
         hinge_inputs = np.multiply(margins, y.reshape(-1, 1))
-
         norm = np.linalg.norm(w)
+        
+        # Adjust hinge_inputs shape to be 2D
+        shaped_hinge_inputs = hinge_inputs.reshape(-1, 1)
+        
+        # create array of 1s and array of 0s, with the same shape as hinge_inputs
+        ones = np.ones(shaped_hinge_inputs.shape)
+        zeros = np.zeros(shaped_hinge_inputs.shape)
+        
+        # Calculate the value for each sample
+        hinge_values = np.subtract(ones, shaped_hinge_inputs)
+        loss = norm**2 + C * np.sum(np.maximum(zeros, hinge_values))
 
-        # TODO: complete the loss calculation
-        loss = 0.0
-
-        return
+        return loss
 
     @staticmethod
     def subgradient(w, b: float, C: float, X, y):
@@ -64,9 +71,18 @@ class SoftSVM(BaseEstimator, ClassifierMixin):
         :param y: targets for loss computation; array of shape (n_samples,)
         :return: a tuple with (the gradient of the weights, the gradient of the bias)
         """
-        # TODO: calculate the analytical sub-gradient of soft-SVM w.r.t w and b
-        g_w = None
-        g_b = 0.0
+        # Compute Raw Decision Values
+        classifier_vector = (np.dot(X, w) + b).reshape(-1, 1)
+        classifier_vector = np.multiply(classifier_vector, y.reshape(-1, 1))
+        
+        # Identify Violations of the Margin and fix them to -1 and 0 for the rest
+        fixed_vector = np.where(classifier_vector < 1, -1, 0)
+        
+        # This line calculates vec1, which will be -1 for misclassified points (where the margin is less than 1), and 0 otherwise.
+        missclassify_vector = np.multiply(fixed_vector, y.reshape(-1, 1))
+        
+        g_w = 2*w + C * np.sum(np.multiply(X, missclassify_vector.reshape(-1, 1)), axis=0)
+        g_b = C * np.sum(np.multiply(fixed_vector,y))
 
         return g_w, g_b
 
@@ -101,13 +117,11 @@ class SoftSVM(BaseEstimator, ClassifierMixin):
             batch_X = X[start_idx:end_idx, :]
             batch_y = y[start_idx:end_idx]
 
-            # TODO: Compute the (sub)gradient of the current *batch*
-            g_w, g_b = None, None
+            g_w, g_b = self.subgradient(self.w, self.b, self.C, batch_X, batch_y)
 
-            # Perform a (sub)gradient step
-            # TODO: update the learned parameters correctly
-            self.w = None
-            self.b = 0.0
+            # Perform a (sub)gradient step (value - learning rate*gradient)
+            self.w = self.w - self.lr*g_w
+            self.b = self.b - self.lr*g_b
 
             if keep_losses:
                 losses.append(self.loss(self.w, self.b, self.C, X, y))
@@ -136,7 +150,8 @@ class SoftSVM(BaseEstimator, ClassifierMixin):
         :return: Predicted class labels for samples in X; array of shape (n_samples,)
                  NOTE: the labels must be either +1 or -1
         """
-        # TODO: compute the predicted labels (+1 or -1)
-        y_pred = None
+        # classifier vector
+        classifier_vector = np.dot(X, self.w) + self.b
+        y_pred = np.sign(classifier_vector)
 
         return y_pred
